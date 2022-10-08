740d88e7bea7b4621c8cf2d196b1bc195c6ad0ea
BigAdroit <adroit819@gmail.com>
2022-10-08 17:38:13 +0100

    vercel cloudinary update

--- src/middlewares/cloudinary.js
+++ src/middlewares/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    cloud_name: "dly3zx7bt",
+    api_key: "559718689289532",
+    api_secret: "IhK-RJRzRjJZgSzHhVa-Oiciiyg"
 })
 
 module.exports = cloudinary
